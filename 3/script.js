7aa49d1140372065dea6bb4c6106f668161037f1
Kari <example@gmail.com>
2021-05-30 14:49:45 +0600

    New third lesson JS

---
+++ 3/script.js
@@ -0,0 +1,13 @@
+/* function showFirstMessage(text) {
+    alert(text)
+}
+
+showFirstMessage('Olofeller') */
+
+function calc(a,b) {
+    return (a + b);
+}
+
+console.log(calc(3,5));
+
+console.log(calc(49,245));
